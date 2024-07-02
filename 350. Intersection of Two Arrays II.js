7ecbd844492b273efb0c38c8d1b429674208ce65
simanto848 <simanto848@gmail.com>
2024-07-02 13:59:22 +0600

    start 30 day of js

---
+++ 350. Intersection of Two Arrays II.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersect = function (nums1, nums2) {
+  const map = {};
+  const result = [];
+
+  for (let num of nums1) {
+    map[num] = (map[num] || 0) + 1;
+  }
+
+  for (let num of nums2) {
+    if (map[num] > 0) {
+      result.push(num);
+      map[num]--;
+    }
+  }
+
+  return result;
+};
